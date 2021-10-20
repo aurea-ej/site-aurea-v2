8377e45462ca0d4e305cda8f58be2e3c120736a8
Higor Gonçalves Brandão <higorb2000@gmail.com>
2021-10-20 18:56:06 -0300

    fix erro do popup

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -2,14 +2,14 @@ import { React, useState } from 'react';
 import { Link } from "react-router-dom";
 
 import Guitarist from '../../imgs/violão.png'
-import RockInAurea3 from '../../imgs/rockInAurea3.png'
-import Pattern from '../../imgs/abstract.png'
+import RockInAurea3 from '../../imgs/rockinaurea4.png'
+import Pattern from '../../imgs/abstract2.jpeg'
 
 import './style.scss';
 
 function Popup() {
 
-    const [displayPopup, setDisplayPopup] = useState('none');
+    const [displayPopup, setDisplayPopup] = useState('flex');
 
     function closePopup() {
 
@@ -30,8 +30,12 @@ function Popup() {
 
                 <div className="infoPopup">
 
-                    <img draggable="false" id="rockInAurea" src={RockInAurea3} alt="" />
-                    <h1>Processo Seletivo 2021.2</h1>
+                    <div className="imgPSWrapper">
+
+                        <img draggable="false" id="rockInAurea" src={RockInAurea3} alt="" />
+                    
+                    </div>
+                    <h1>Processo Seletivo <br/>2021.2</h1>
                     <Link className="buttonInfo" to="/processo-seletivo">Saiba mais</Link>
 
                 </div>
